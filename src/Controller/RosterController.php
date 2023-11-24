dcb8e716100f0a4cf61cd5550ea3f7924f9c6862
amtbzh@gmail.com <amtbzh@gmail.com>
2023-11-24 09:45:31 +0100

    add: Symfony

---
+++ src/Controller/RosterController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RosterController extends AbstractController
+{
+    #[Route('/roster', name: 'roster.index')]
+    public function index(): Response
+    {
+        return $this->render('roster/index.html.twig');
+    }
+}
